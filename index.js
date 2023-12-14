0ce7d6a3f73db0c32a9f64c31199d9d2d95bf570
miriam <miriamwangui525@gmail.com>
2023-12-14 10:49:30 +0300

    how to do strings in javascript

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+console.log('currentUser:', currentUser);
+console.log('welcomeMessage:', welcomeMessage);
+console.log('excitedWelcomeMessage:', excitedWelcomeMessage);
+console.log('shortGreeting:', shortGreeting);
+
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting,
+};
